8b88777d2dbe4ca2eaacf9a2662046c18b4dbf7b
wasemjbaren <wasemjbaren@gmail.com>
2022-04-14 17:53:01 +0300

    first upload system project

---
+++ Migrations/202204072226410_AddCoursesTable2.cs
@@ -0,0 +1,18 @@
+﻿namespace IdintityProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCoursesTable2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CourseModels", "CourseDescription", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CourseModels", "CourseDescription");
+        }
+    }
+}
