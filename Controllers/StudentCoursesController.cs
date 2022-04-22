4a5b77ecdfeee5d636d336399ddf61bdcfd6de4f
wasemjbaren <wasemjbaren@gmail.com>
2022-04-22 19:11:10 +0300

    Show Courses of student -Rani

---
+++ Controllers/StudentCoursesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IdintityProject.Models;
+
+namespace IdintityProject.Controllers
+{
+    public class StudentCoursesController : Controller
+    {
+        public ApplicationDbContext db;
+
+        public StudentCoursesController()
+        {
+            db = new ApplicationDbContext();
+        }
+        // GET: StudentCourses
+        public ActionResult Index()
+        {
+            var lst =db.GradesModel.Where(d => d.Student_Name == User.Identity.Name).ToList();
+            return View(lst);
+        }
+
+       
+        
+
+        /* Do here: בקשת סטודנט להוסף קורס מסוים*/
+
+
+    }
+}
